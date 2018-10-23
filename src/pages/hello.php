e81b9c213844fbdda1aa358981b9a4c737cbb894
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-10-23 14:36:09 +0200

    extract query parameter in our router

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,3 +1,3 @@
 <!-- now the page converted to template -->
-<?php $name = $request->get('name', 'World'); ?>
-Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
+
+Hello <?= htmlspecialchars(isset($name)? $name: 'World', ENT_QUOTES, 'UTF-8') ?>
